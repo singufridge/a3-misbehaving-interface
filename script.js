3498aea3da8d86a46196f98dbd2a2cf0edb8065b
singufridge <cadence.delia382@gmail.com>
2026-05-21 14:22:21 -0400

    added comments

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // button functions
 
+// changes background image
 function bgChange(url) {
     document.body.style.backgroundImage = `url(https://` + url + `)`;
 }
 
+// changes text colour when pushing the green button
 function greenBtn() {
     document.body.style.color = `green`;
 }
 
+// add event listener for blue button, call btnReplace
 document.getElementById("blue-btn").addEventListener("click", btnReplace)
 
+// function is called when clicking blue button, removes button and replaces it with an image of a blueberry
 function btnReplace() {
     document.getElementById("blue-btn").remove();
 
@@ -21,10 +25,12 @@ function btnReplace() {
     document.getElementById("blue-btn-div").appendChild(blueImg);
 }
 
+// function for purple button, replaces text with glitched text
 document.getElementById("purple-btn").addEventListener("click", function() {
     document.getElementById("purple-btn").innerHTML = 'P̴̢̨̧̧̻̼̭͍̣̘̣̥̖̂̓́̀͆̊̃͌̎̈̌̀̀̾̈́͛̈́͑͌̒̕͜͝ͅư̵͎̓͛̆̃́̿̇̋̊̀͗̄̋̿͊͘͘͜r̵̢̮̯̱̪͔̖̫͇̣͇̭̻̹̫̗̙̯͚͕̜̻̻̹̭̹̫͓͖̤͊̋̆̓͒̽̏̋̀̇̃͛̌̀̂́͑͗̎̚͠͠ͅp̷̢̢̢̡̨̡̡̗̻̘͉͈̟̥̯̘̱͓̗͈̘̬͙̜̹̳͓̮͙̗̼͚̝̂͐̍̽͐̒̀̑̎̊́̄́̊̎̎̽̀̄͗̚̚̕͜͠͠l̴͕̙̑́̍̈́̈̿̐̐̿̎̓̀̌͌̽̚͝e̷̢̨̡̡̨̧͍͙̘̰͖͇̺̥̤̬͇͕̹̖̰͖̹͍̙͎̜̻̭̲͙̾̓̆͂̓́͐̑͊͒̑̈̋̆͗̃̾̄̓̿͝͝͠';
 })
 
+// function that makes a message appear when it was previously invisible, uses parameter to identify ID from the html side
 function errMsg(errID) {
     document.getElementById(errID).style.opacity = 100;
 }
