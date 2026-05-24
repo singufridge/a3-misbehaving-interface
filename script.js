6b7ebd9ac5728f0ec72eeb2410288cedb6e3f6ae
singufridge <cadence.delia382@gmail.com>
2026-05-24 16:03:59 -0400

    attempting to make button expand

--- script.js
+++ script.js
@@ -4,15 +4,15 @@
 // changes background image
 function bgChange(url) {
     document.body.style.backgroundImage = `url(https://` + url + `)`;
-}
+};
 
 // changes text colour when pushing the green button
 function greenBtn() {
     document.body.style.color = `green`;
-}
+};
 
 // function is called when clicking blue button, removes button and replaces it with an image of a blueberry
-document.getElementById("blue-btn").addEventListener("click", btnReplace)
+document.getElementById("blue-btn").addEventListener("click", btnReplace);
 
 function btnReplace() {
     document.getElementById("blue-btn").remove();
@@ -23,7 +23,7 @@ function btnReplace() {
     blueImg.alt = 'Blueberry';
 
     document.getElementById("blue-btn-div").appendChild(blueImg);
-}
+};
 
 // function for purple button, replaces text with glitched text
 const purpleBtn = document.getElementById("purple-btn");
@@ -31,7 +31,7 @@ const purpleBtn = document.getElementById("purple-btn");
 purpleBtn.addEventListener("click", function purpleBtnChange() {
     purpleBtn.innerHTML = 'P̴̢̨̧̧̻̼̭͍̣̘̣̥̖̂̓́̀͆̊̃͌̎̈̌̀̀̾̈́͛̈́͑͌̒̕͜͝ͅư̵͎̓͛̆̃́̿̇̋̊̀͗̄̋̿͊͘͘͜r̵̢̮̯̱̪͔̖̫͇̣͇̭̻̹̫̗̙̯͚͕̜̻̻̹̭̹̫͓͖̤͊̋̆̓͒̽̏̋̀̇̃͛̌̀̂́͑͗̎̚͠͠ͅp̷̢̢̢̡̨̡̡̗̻̘͉͈̟̥̯̘̱͓̗͈̘̬͙̜̹̳͓̮͙̗̼͚̝̂͐̍̽͐̒̀̑̎̊́̄́̊̎̎̽̀̄͗̚̚̕͜͠͠l̴͕̙̑́̍̈́̈̿̐̐̿̎̓̀̌͌̽̚͝e̷̢̨̡̡̨̧͍͙̘̰͖͇̺̥̤̬͇͕̹̖̰͖̹͍̙͎̜̻̭̲͙̾̓̆͂̓́͐̑͊͒̑̈̋̆͗̃̾̄̓̿͝͝͠';
     purpleBtn.style.color = 'purple';
-})
+});
 
 // * NAV PICKER
 // prevents form submission from reloading the page and reveals navbar
@@ -39,12 +39,12 @@ document.getElementById('nav-picker-form').addEventListener('submit', function g
     event.preventDefault();
     
     document.getElementById("navbar").style.opacity = 100;
-})
+});
 
 // function that makes a message appear when it was previously invisible, uses parameter to identify ID from the html side
 function errMsg(errID) {
     document.getElementById(errID).style.opacity = 100;
-}
+};
 
 // replace all characters with 'e' when trying to type in prompt in form
 document.getElementById('nav-type').addEventListener('input', (e) => {
@@ -74,19 +74,42 @@ navPopForm.addEventListener('submit', function genNavLinks() {
     navLink.classList.add("a-animated"); //a dd class to 'a' element that causes it to be animated
     navLink.addEventListener("click", () => changeClass(navLink)); // add onclick event listener to the generated item asynchronously
     navLi.appendChild(navLink);
-})
+});
 
 let clickCount = 1;
 function changeClass(navLink) { // on click, remove css class that animates the link element  
     navLink.classList.remove('a-animated');
 
-    document.body.style.transform = 'rotate(' + (clickCount * 6) + 'deg)';
+    document.body.style.transform = `rotate(${clickCount * 6}deg)`;
 
     clickCount++;
-}
+};
 
 // * PUBLISH BUTTON
 // grows continuously bigger while you hover over it
 const pubBtn = document.getElementById('pub-btn');
 
 let isHovering = false;
+let animationFrameId = null;
+
+let scale = 1.0;
+
+pubBtn.addEventListener('hover', function pubBtnExpand() {
+    if (isHovering) {
+        scale += 0.2;
+        pubBtn.style.transform = `scale(${scale})`;
+
+        animationFrameId = requestAnimationFrame(pubBtnExpand);
+    };
+});
+
+pubBtn.addEventListener('mouseenter', () => {
+    isHovered = true;
+    cancelAnimationFrame(animationFrameId);
+    requestAnimationFrame(pubBtnExpand());
+});
+
+pubBtn.addEventListener('mouseleave', () => {
+    isHovered = false;
+    cancelAnimationFrame(animationFrameId);
+});
